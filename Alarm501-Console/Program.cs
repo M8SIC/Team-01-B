7a6a3a4cd0f42b2d9c71ee1d8bb9ecaf38af68c3
colej <colej5095@ksu.edu>
2025-03-11 21:23:32 -0500

    fixed duplicated delegates across projects

--- Alarm501-Console/Program.cs
+++ Alarm501-Console/Program.cs
@@ -2,13 +2,6 @@
 
 namespace Alarm501_Console
 {
-    public delegate void ParameterlessFunc();
-    public delegate void PassListOfIntFunc(List<int> i);
-    public delegate void SetButtonEnableTo(bool enabled);
-    public delegate int GetCurrentSelectedIndex();
-    public delegate BindingList<Alarm>? GetAlarmList();
-    public delegate void SendAlarmFunc(Alarm alarm);
-    public delegate void SendAlarmFuncWithSnoozeTime(Alarm alarm, int snoozeTime);
     public class Program
     {
         public static void Main(string[] args)
